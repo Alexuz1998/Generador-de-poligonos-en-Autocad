a80c5bfdc3ecbb8fcd4ead42c672157287d211e5
Alexuz1998 <155028422+Alexuz1998@users.noreply.github.com>
2026-09-19 01:16:14 -0500

    Update app.py

--- app.py
+++ app.py
@@ -292,8 +292,8 @@ hr {
 }
 
 .atlas-footer-gif {
-    width: 90x !important;
-    height: 90px !important;
+    width: 150x !important;
+    height: 150px !important;
     max-width: 80px !important;
     max-height: 80px !important;
     object-fit: contain;
