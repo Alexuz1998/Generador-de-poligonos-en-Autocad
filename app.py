d55c31ec952fb861bb3499d8b33ac03802f2b489
Alexuz1998 <155028422+Alexuz1998@users.noreply.github.com>
2026-09-18 23:05:34 -0500

    Update app.py

--- app.py
+++ app.py
@@ -268,8 +268,8 @@ hr {
 }
 
 .atlas-title-pikachu {
-    width: 100px;
-    height: 100px;
+    width: 120px;
+    height: 120px;
     display: block;
     flex: 0 0 60px;
     object-fit: contain;
